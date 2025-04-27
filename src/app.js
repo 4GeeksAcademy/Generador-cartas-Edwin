b5eb6c307e582621a9deddd9d3144034716a7372
Edwin González <130111056+edslogia@users.noreply.github.com>
2025-04-27 20:44:48 +0000

    valor de carta aleatorio asignado

--- src/app.js
+++ src/app.js
@@ -4,5 +4,11 @@ import "./style.css";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+
+  let valorCarta = ["A", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+  let numeroAleatorio = Math.floor(Math.random() * 13)  
+  document.getElementById("valor-carta").innerHTML = valorCarta[numeroAleatorio];
+
+
+
 };
