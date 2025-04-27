ea5f7beac069010ef3d012a1f807fbc7b6a79c02
Edwin González <130111056+edslogia@users.noreply.github.com>
2025-04-27 22:38:03 +0000

    ejercicio terminado a nivel basico

--- src/app.js
+++ src/app.js
@@ -6,9 +6,17 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
 
   let valorCarta = ["A", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
-  let numeroAleatorio = Math.floor(Math.random() * 13)  
-  document.getElementById("valor-carta").innerHTML = valorCarta[numeroAleatorio];
+  let numeroAleatorioCarta = Math.floor(Math.random() * 13); 
+  document.getElementById("valor-carta").innerHTML = valorCarta[numeroAleatorioCarta];
 
+  let pintas = ["spade", "heart", "club", "diamond"];
+  let numeroAleatorioPintas = Math.floor(Math.random() * 4);
+  
+  let pintaUp = document.querySelector(".pinta-up");
+  let pintaDown = document.querySelector(".pinta-down");
 
+  pintaUp.className = `pinta-up ${pintas[numeroAleatorioPintas]}`;
+  pintaDown.className = `pinta-down ${pintas[numeroAleatorioPintas]}`;
+  console.log()
 
 };
