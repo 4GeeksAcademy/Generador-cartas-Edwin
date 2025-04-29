156b724c864295ca9829e65e529886051709e78e
Edwin González <130111056+edslogia@users.noreply.github.com>
2025-04-29 17:40:31 +0000

    se agregan límites de ancho y alto

--- src/app.js
+++ src/app.js
@@ -4,8 +4,23 @@ import "./style.css";
 import "./assets/img/4geeks.ico";
 
 function generarCarta() {
-  let valorCarta = ["A", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
-  let numeroAleatorioCarta = Math.floor(Math.random() * 13); 
+  let valorCarta = [
+    "A",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+  ];
+  let numeroAleatorioCarta = Math.floor(Math.random() * 13);
 
   let pintas = ["spade", "heart", "club", "diamond"];
   let numeroAleatorioPintas = Math.floor(Math.random() * 4);
@@ -14,9 +29,8 @@ function generarCarta() {
 }
 
 function actualizarCarta() {
-  
   let [carta, pinta] = generarCarta();
-  
+
   document.getElementById("valor-carta").innerHTML = carta;
 
   let pintaUp = document.querySelector(".pinta-up");
@@ -46,23 +60,40 @@ function ajustarTamañoCarta() {
   const inputAlto = document.getElementById("alto-carta");
   const carta = document.querySelector(".card");
 
-    inputAncho.addEventListener("input", () => {
+  // Límites para el ancho y alto
+  const minAncho = 100;
+  const maxAncho = 1000;
+  const minAlto = 150;
+  const maxAlto = 700;
+
+  inputAncho.addEventListener("input", () => {
     const ancho = parseInt(inputAncho.value, 10);
-    if (!isNaN(ancho) && ancho > 0) {
-      carta.style.width = `${ancho}px`;
+
+    if (isNaN(ancho) || ancho < minAncho) {
+      ancho = minAncho;
+    } else if (ancho > maxAncho) {
+      ancho = maxAncho;
     }
+
+    inputAncho.value = ancho; 
+    carta.style.width = `${ancho}px`; 
   });
 
   inputAlto.addEventListener("input", () => {
     const alto = parseInt(inputAlto.value, 10);
-    if (!isNaN(alto) && alto > 0) {
-      carta.style.height = `${alto}px`;
+
+    if (isNaN(alto) || alto < minAlto) {
+      alto = minAlto;
+    } else if (alto > maxAlto) {
+      alto = maxAlto;
     }
+
+    inputAlto.value = alto; 
+    carta.style.height = `${alto}px`; 
   });
 }
 
-window.onload = function() {
-  
+window.onload = function () {
   actualizarCarta();
 
   let botonNuevaCarta = document.querySelector(".new-card-button");
